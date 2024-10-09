8e18bbcc0905bb98c2e20a0273e0c29a9a477839
agpsl <agnaldo.silva.mg@gmail.com>
2024-10-09 11:21:46 -0300

    Fix Credit Repository

--- src/main/kotlin/io/github/agpsl/credit_application_system/repository/CreditRepository.kt
+++ src/main/kotlin/io/github/agpsl/credit_application_system/repository/CreditRepository.kt
@@ -1,6 +1,6 @@
 package io.github.agpsl.credit_application_system.repository
 
-import io.github.agpsl.credit_application_system.entity.Customer
+import io.github.agpsl.credit_application_system.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CreditRepository : JpaRepository<Customer, Long>
+interface CreditRepository : JpaRepository<Credit, Long>
