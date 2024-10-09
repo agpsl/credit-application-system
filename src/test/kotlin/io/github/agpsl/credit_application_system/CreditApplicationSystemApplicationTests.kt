daaa3715b4f342871582bc84e88f65ce4725be04
agpsl <agnaldo.silva.mg@gmail.com>
2024-10-09 10:54:30 -0300

    Add default test file

---
+++ src/test/kotlin/io/github/agpsl/credit_application_system/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.agpsl.credit_application_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
