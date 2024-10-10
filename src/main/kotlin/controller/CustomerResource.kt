567f8683de9afdab654bb94e257a412a2ad2791f
agpsl <agnaldo.silva.mg@gmail.com>
2024-10-10 10:21:51 -0300

    Create updateCustomer method

--- src/main/kotlin/controller/CustomerResource.kt
+++ src/main/kotlin/controller/CustomerResource.kt
@@ -1,6 +1,7 @@
 package controller
 
 import dto.CustomerDto
+import dto.CustomerUpdateDto
 import dto.CustomerView
 import io.github.agpsl.credit_application_system.service.impl.CustomerService
 import org.springframework.web.bind.annotation.*
@@ -24,4 +25,13 @@ class CustomerResource(
 
     @DeleteMapping("/{id}")
     fun deleteCustomer(@PathVariable id: Long) = customerService.delete(id)
+
+    @PatchMapping("/{id}")
+    fun updateCustomer(@RequestParam(value = "customerId") id: Long,
+                       @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView {
+        val customer = this.customerService.findById(id)
+        val customerToUpdate = customerUpdateDto.toEntity(customer)
+        val customerUpdated = this.customerService.save(customerToUpdate)
+        return CustomerView(customerUpdated)
+    }
 }
