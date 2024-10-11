d0f03206aa1b172e266cce5bdd06b9f694349076
agpsl <agnaldo.silva.mg@gmail.com>
2024-10-11 16:17:52 -0300

    Implement business rules

--- src/main/kotlin/io/github/agpsl/credit_application_system/service/impl/CreditService.kt
+++ src/main/kotlin/io/github/agpsl/credit_application_system/service/impl/CreditService.kt
@@ -1,9 +1,12 @@
 package io.github.agpsl.credit_application_system.service.impl
 
 import io.github.agpsl.credit_application_system.entity.Credit
+import io.github.agpsl.credit_application_system.exception.BusinessException
 import io.github.agpsl.credit_application_system.repository.CreditRepository
 import io.github.agpsl.credit_application_system.service.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 @Service
@@ -12,7 +15,19 @@ class CreditService(
     private val customerService: CustomerService
 ) : ICreditService {
 
+    /**
+    *  Business Rules
+    * - date of first installment should not be less than 3 months from now
+    * - number of installments should be less than or equal to 48
+    */
     override fun save(credit: Credit): Credit {
+        val firstInstallmentInDays = LocalDate.now().until(credit.dayFirstInstallment, ChronoUnit.DAYS)
+
+        if (firstInstallmentInDays > 90)
+            throw BusinessException("First installment should be no less than 90 days from now")
+        else if (credit.numberOfInstallments > 48)
+            throw BusinessException("Too many installments")
+
         credit.apply { customer = customerService.findById(credit.customer?.id!!) }
         return creditRepository.save(credit)
     }
